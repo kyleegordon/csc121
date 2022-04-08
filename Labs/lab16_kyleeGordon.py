bba4b255c2d2e55aae3d565012215d4823b3ebca
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-04-08 09:56:50 -0400

    lab 16

---
+++ Labs/lab16_kyleeGordon.py
@@ -0,0 +1,35 @@
+# Kylee Gordon Lab-17 04/08/2022
+
+# This program gets user input and uses it to generate a html file containing the code required to
+# create a simple website containing the user's input
+
+def main():
+    # Input
+    name = input("Enter your name: ")
+    description = input("Describe yourself: ")
+
+    # Processing
+    create_file()
+    html = generate_html(name, description)
+    write_file(html)
+
+    # Output
+    print("You webpage has been created, the HTML can be viewed in the file: webpage.html")
+
+
+def create_file():
+    file = open("webpage.html", "w")
+    file.close()
+
+
+def write_file(html):
+    file = open("webpage.html", "w")
+    file.write(html)
+
+
+def generate_html(name, description):
+    html = "<html>\n<head>\n</head>\n<body>\n<center>\n<h1>\n" + name + "\n</h1>\n</center>\n<hr />\n" + description + "\n<hr />\n</body>\n</html> "
+    return html
+
+
+main()
