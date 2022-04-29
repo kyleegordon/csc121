03f4e3007a2ebdbd62e4e80b11d861060ee01e24
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-04-29 12:36:42 -0400

    finish lab 21

--- Labs/lab21_kyleeGordon.py
+++ Labs/lab21_kyleeGordon.py
@@ -8,30 +8,50 @@ import csv
 def main():
     # Setup
     filename = "lab21_excel.csv"
-    headers = []
     rows = []
 
     # Input
     with open(filename, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
 
-        headers = next(csvreader)
-
         for row in csvreader:
             rows.append(row)
 
-    # Processing/Output
-    # Print headers
-    for col in headers:
-        print("%-15s" % col, end=" "),
-    print('\n')
+    overall_total = 0
 
+    # Processing/Output
     # Print rows
-    for row in rows[:csvreader.line_num]:
-        row_total = 0
+    for row in rows[:csvreader.line_num - 1]:
         for col in row:
-            print("%-15s" % col, end=" "),
+            if col != "0.00":
+                print("%-15s" % col, end=" "),
+            else:
+                total = calculate_row_total(row)
+                print("%-15s" % total, end=" "),
+                overall_total += total
         print('\n')
+
+    # Print column totals
+    print("%-15s" % "Total", end=" ")
+    for x in range(1, 6):
+        col_total = calculate_column_total(rows, x)
+        print("%-15s" % col_total, end=" "),
+
+    print("%-15s" % overall_total, end=" "),
+
+
+def calculate_row_total(number_array):
+    total = 0
+    for x in range(1, len(number_array) - 1):
+        total += float(number_array[x])
+    return total
+
+
+def calculate_column_total(rows, column_num):
+    col_total = 0
+    for x in range(1, len(rows)):
+        col_total += float(rows[x][column_num])
+    return col_total
 
 
 main()
